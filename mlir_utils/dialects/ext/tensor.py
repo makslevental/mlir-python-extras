e5846dc06372fac1cdca52a4c71dbcd7bcc5106b
max <maksim.levental@gmail.com>
2023-08-08 18:58:51 -0500

    start parfors

--- mlir_utils/dialects/ext/tensor.py
+++ mlir_utils/dialects/ext/tensor.py
@@ -618,3 +618,44 @@ def _insert_slice(
         raise ValueError(f"non-constant indices not supported {indexer}")
 
     return out
+
+
+def parallel_insert_slice(
+    source,
+    dest,
+    offsets=None,
+    sizes=None,
+    strides=None,
+    static_offsets=None,
+    static_sizes=None,
+    static_strides=None,
+):
+    if static_offsets is None:
+        assert offsets is not None
+        static_offsets = [S, S]
+    if static_sizes is None:
+        assert sizes is not None
+        static_sizes = [S, S]
+    if static_strides is None:
+        assert strides is not None
+        static_strides = [S, S]
+    if offsets is None:
+        assert static_offsets
+        offsets = []
+    if sizes is None:
+        assert static_sizes
+        sizes = []
+    if strides is None:
+        assert static_strides
+        strides = []
+
+    return tensor.parallel_insert_slice(
+        source,
+        dest,
+        offsets,
+        sizes,
+        strides,
+        static_offsets,
+        static_sizes,
+        static_strides,
+    )
