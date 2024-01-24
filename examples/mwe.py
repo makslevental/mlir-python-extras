0ec30919254b719e29aa2c8267722826d1fcb8c8
Maksim Levental <maksim.levental@gmail.com>
2024-01-24 13:03:18 -0600

    assert the right thing... (#53)

--- examples/mwe.py
+++ examples/mwe.py
@@ -1,52 +1,53 @@
+import mlir.extras.types as T
 import numpy as np
 
 # you need this to register the memref value caster
 # noinspection PyUnresolvedReferences
 import mlir.extras.dialects.ext.memref
-import mlir.extras.types as T
 from mlir.extras.ast.canonicalize import canonicalize
-from mlir.extras.context import MLIRContext, mlir_mod_ctx
+from mlir.extras.context import RAIIMLIRContext, ExplicitlyManagedModule
 from mlir.extras.dialects.ext.arith import constant
 from mlir.extras.dialects.ext.func import func
 from mlir.extras.dialects.ext.scf import canonicalizer as scf, range_ as range
 from mlir.extras.runtime.passes import Pipeline
 from mlir.extras.runtime.refbackend import LLVMJITBackend
 
+ctx = RAIIMLIRContext()
+backend = LLVMJITBackend()
+module = ExplicitlyManagedModule()
 
-def setting_memref(ctx: MLIRContext, backend: LLVMJITBackend):
-    K = 10
-    memref_i64 = T.memref(K, K, T.i64())
-
-    @func
-    @canonicalize(using=scf)
-    def memfoo(A: memref_i64, B: memref_i64, C: memref_i64):
-        one = constant(1)
-        two = constant(2)
-        if one > two:
-            three = constant(3)
-        else:
-            for i in range(0, K):
-                for j in range(0, K):
-                    C[i, j] = A[i, j] * B[i, j]
-
-    memfoo.emit()
-    # run_pipeline(ctx.module, Pipeline().cse())
-    print(ctx.module)
-
-    module = backend.compile(
-        ctx.module,
-        kernel_name=memfoo.__name__,
-        pipeline=Pipeline().bufferize().lower_to_llvm(),
-    )
-
-    # windows defaults to int32
-    A = np.random.randint(0, 10, (K, K)).astype(np.int64)
-    B = np.random.randint(0, 10, (K, K)).astype(np.int64)
-    C = np.zeros((K, K), dtype=np.int64)
-
-    backend.load(module).memfoo(A, B, C)
-    assert np.array_equal(A * B, C)
+K = 10
+memref_i64 = T.memref(K, K, T.i64())
 
 
-with mlir_mod_ctx() as ctx:
-    setting_memref(ctx, LLVMJITBackend())
+@func
+@canonicalize(using=scf)
+def memfoo(A: memref_i64, B: memref_i64, C: memref_i64):
+    one = constant(1)
+    two = constant(2)
+    if one > two:
+        three = constant(3)
+    else:
+        for i in range(0, K):
+            for j in range(0, K):
+                C[i, j] = A[i, j] * B[i, j]
+
+
+memfoo.emit()
+module = module.finish()
+
+print(module)
+
+module = backend.compile(
+    module,
+    kernel_name=memfoo.__name__,
+    pipeline=Pipeline().bufferize().lower_to_llvm(),
+)
+
+# windows defaults to int32
+A = np.random.randint(0, 10, (K, K)).astype(np.int64)
+B = np.random.randint(0, 10, (K, K)).astype(np.int64)
+C = np.zeros((K, K), dtype=np.int64)
+
+backend.load(module).memfoo(A, B, C)
+assert np.array_equal(A * B, C)
