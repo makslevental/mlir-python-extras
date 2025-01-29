80b8fd3626e853de8ccefb1e1cbd65d7dcc6a71d
Maksim Levental <maksim.levental@gmail.com>
2025-01-29 16:14:05 -0500

    call_instrinsic

---
+++ tests/test_llvm.py
@@ -0,0 +1,37 @@
+from textwrap import dedent
+
+import pytest
+
+import mlir.extras.types as T
+from mlir.extras.dialects.ext import llvm
+from mlir.extras.dialects.ext.func import func
+
+# noinspection PyUnresolvedReferences
+from mlir.extras.testing import MLIRContext, filecheck, mlir_ctx as ctx
+
+# needed since the fix isn't defined here nor conftest.py
+pytest.mark.usefixtures("ctx")
+
+
+def test_call_instrinsic(ctx: MLIRContext):
+    @func(emit=True)
+    def sum(a: T.i32(), b: T.i32(), c: T.f32()):
+        llvm.call_intrinsic(
+            a, b, intr_id=2074, intr_name="llvm.amdgcn.cvt_pk_i16", is_overloaded=False
+        )
+        e = llvm.amdgcn.cvt_pk_i16(a, b)
+        f = llvm.amdgcn.frexp_mant(c)
+
+    correct = dedent(
+        """
+    module {
+      func.func @sum(%arg0: i32, %arg1: i32, %arg2: f32) {
+        %0 = llvm.call_intrinsic "llvm.amdgcn.cvt_pk_i16"(%arg0, %arg1) : (i32, i32) -> vector<2xi16>
+        %1 = llvm.call_intrinsic "llvm.amdgcn.cvt.pk.i16"(%arg0, %arg1) : (i32, i32) -> vector<2xi16>
+        %2 = llvm.call_intrinsic "llvm.amdgcn.frexp.mant"(%arg2) : (f32) -> f32
+        return
+      }
+    }
+    """
+    )
+    filecheck(correct, ctx.module)
