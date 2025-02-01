94f480cd84bc8c03b19cc2ca4d5e9aecee11dcb7
Maksim Levental <maksim.levental@gmail.com>
2025-02-01 06:16:09 -0500

    Test sym vis

--- tests/test_other_hosts.py
+++ tests/test_other_hosts.py
@@ -5,6 +5,8 @@ from util import jax_not_installed, mlir_bindings_not_installed
 
 @pytest.mark.skipif(jax_not_installed(), reason="jax not installed")
 def test_jax_trampolines_smoke():
+    from mlir import ir
+    from jaxlib.mlir import ir
     # noinspection PyUnresolvedReferences
     from jaxlib.mlir.extras import context
     # noinspection PyUnresolvedReferences
