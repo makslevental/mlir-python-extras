d8f978a84f58ff53c954b80ab075baff6a1d7262
Maksim Levental <maksim.levental@gmail.com>
2025-03-31 18:23:49 -0400

    fix launch_kernel

--- examples/rdna_matmul_opt.py
+++ examples/rdna_matmul_opt.py
@@ -1,13 +1,11 @@
-import time
-
-import mlir.extras.types as T
 import numpy as np
 from hip import hip
 
 from mlir.extras.ast.canonicalize import canonicalize
 from mlir.extras.context import RAIIMLIRContextModule
-from mlir.extras.dialects.ext import memref, scf, arith, gpu
-from mlir.dialects import rocdl
+from mlir.extras.dialects.ext import memref, scf, arith, gpu, llvm
+from mlir.ir import InsertionPoint, IntegerAttr
+import mlir.extras.types as T
 
 # noinspection PyUnresolvedReferences
 from mlir.extras.dialects.ext.gpu import (
@@ -27,6 +25,7 @@ from mlir.extras.dialects.ext.gpu import (
     lds_space,
 )
 from mlir.extras.runtime.passes import run_pipeline, Pipeline
+from mlir.extras.util import find_ops
 
 # noinspection PyUnresolvedReferences
 from util import hip_check, launch_kernel, hip_synchronize
@@ -38,10 +37,24 @@ _ = memref
 ctx = RAIIMLIRContextModule()
 set_container_module(ctx.module)
 
-M, K, N = 16, 16, 16
+props = hip.hipDeviceProp_t()
+hip_check(hip.hipGetDeviceProperties(props, 0))
+arch = props.gcnArchName.decode()
 
 
-@gpu_func
+# just a default attr - actual target is set blow
+@module("kernels", [f"#rocdl.target"])
+def gpu_module():
+    pass
+
+
+ip = InsertionPoint.at_block_begin(gpu_module.regions[0].blocks[0])
+ip.__enter__()
+
+M, K, N = 1024, 1024, 1024
+
+
+@gpu_func(emit=True)
 @canonicalize(using=scf.canonicalizer)
 def kernel1_naive(
     A: T.memref(M, K, T.f32()), B: T.memref(K, N, T.f32()), C: T.memref(M, N, T.f32())
@@ -53,87 +66,377 @@ def kernel1_naive(
     ):
         acc = arith.constant(0.0)
         for k, acc, _ in scf.range_(K, iter_args=[acc]):
-            acc += A[row, k] * B[k, col]
+            a = A[row, k]
+            b = B[k, col]
+            acc = llvm.intr_fmuladd(a, b, acc)
             acc = yield acc
+
         C[row, col] = acc
 
 
-BN = BK = TILE_SIZE = 16
+launch_params = {
+    kernel1_naive.__name__: (
+        (N // 16, N // 16, 1),
+        (16, 16, 1),
+        0,
+    )
+}
+
+BN = BK = TILE_SIZE = 32
+
+A_shared = memref.global_(
+    sym_name="A_shared_BN_BK_0",
+    type=T.memref(BN, BK, T.f32(), memory_space=lds_space()),
+    alignment=16,
+)
+B_shared = memref.global_(
+    sym_name="B_shared_BK_BN_0",
+    type=T.memref(BK, BN, T.f32(), memory_space=lds_space()),
+    alignment=16,
+)
+
 dtype = T.f32()
 
 
-@gpu_func
+@gpu_func(emit=True)
 @canonicalize(using=scf.canonicalizer)
-def kernel2_lds(
+def kernel2_lds_shared_direct_load_globals(
     A: T.memref(M, K, T.f32()), B: T.memref(K, N, T.f32()), C: T.memref(M, N, T.f32())
 ):
-    As = memref.get_global("A_shared")
-    Bs = memref.get_global("B_shared")
+    As = memref.get_global(A_shared)
+    Bs = memref.get_global(B_shared)
 
-    thread_row = thread_idx.y
-    thread_col = thread_idx.x
+    tid_y = thread_idx.y
+    tid_x = thread_idx.x
 
-    row = block_idx.y * TILE_SIZE + thread_row
-    col = block_idx.x * TILE_SIZE + thread_col
+    row = block_idx.y * TILE_SIZE + tid_y
+    col = block_idx.x * TILE_SIZE + tid_x
 
     sum = arith.constant(0.0)
 
     for t, sum, _ in scf.range_(0, N, BK, iter_args=[sum]):
-        As[thread_row, thread_col] = A[row, t + thread_col]
-        Bs[thread_row, thread_col] = B[t + thread_row, col]
+        Bs[tid_y, tid_x] = B[tid_y + t, col]
+        As[tid_y, tid_x] = A[row, tid_x + t]
 
-        rocdl.s_barrier()
+        gpu.barrier()
 
-        for k, sum, _ in scf.range_(K, iter_args=[sum]):
-            sum += As[thread_row, k] * Bs[k, thread_col]
-            sum = yield sum
+        for k in range(BK):
+            a = As[tid_y, k]
+            b = Bs[k, tid_x]
+            sum = llvm.intr_fmuladd(a, b, sum)
 
-        rocdl.s_barrier()
+        gpu.barrier()
+
         sum = yield sum
 
     C[row, col] = sum
 
 
-props = hip.hipDeviceProp_t()
-hip_check(hip.hipGetDeviceProperties(props, 0))
-arch = props.gcnArchName.decode()
+launch_params[kernel2_lds_shared_direct_load_globals.__name__] = (
+    (N // TILE_SIZE, N // TILE_SIZE, 1),
+    (TILE_SIZE, TILE_SIZE, 1),
+    0,
+)
 
 
-@module("kernels", [f'#rocdl.target<chip = "{arch}">'])
-def gpu_module():
-    A_shared = memref.global_(
-        sym_name="A_shared",
-        type=T.memref(TILE_SIZE, TILE_SIZE, T.f32(), memory_space=lds_space()),
+@gpu_func(emit=True)
+@canonicalize(using=scf.canonicalizer)
+def kernel2_lds_shared_direct_dynamic(
+    A: T.memref(M, K, T.f32()), B: T.memref(K, N, T.f32()), C: T.memref(M, N, T.f32())
+):
+    As = memref.get_global(A_shared)
+    Bs = memref.get_global(B_shared)
+
+    tid_y = thread_idx.y
+    tid_x = thread_idx.x
+
+    row = block_idx.y * TILE_SIZE + tid_y
+    col = block_idx.x * TILE_SIZE + tid_x
+
+    sum = arith.constant(0.0)
+
+    for t, sum, _ in scf.range_(0, N, BK, iter_args=[sum]):
+        Bs[tid_y, tid_x] = B[tid_y + t, col]
+        As[tid_y, tid_x] = A[row, tid_x + t]
+
+        gpu.barrier()
+
+        for k in range(BK):
+            a = As[tid_y, k]
+            b = Bs[k, tid_x]
+            sum = llvm.intr_fmuladd(a, b, sum)
+
+        gpu.barrier()
+
+        sum = yield sum
+
+    C[row, col] = sum
+
+
+launch_params[kernel2_lds_shared_direct_dynamic.__name__] = (
+    (N // TILE_SIZE, N // TILE_SIZE, 1),
+    (TILE_SIZE, TILE_SIZE, 1),
+    2 * TILE_SIZE * TILE_SIZE * T.f32().width // 8,
+)
+
+
+@gpu_func(emit=True)
+@canonicalize(using=scf.canonicalizer)
+def kernel2_lds_shared_subview(
+    A: T.memref(M, K, T.f32()), B: T.memref(K, N, T.f32()), C: T.memref(M, N, T.f32())
+):
+    # allocate buffer for current block in fast shared mem
+    # shared mem is shared between all threads in a block
+    base = gpu.dynamic_shared_memory()
+    As = memref.view(base, (TILE_SIZE, TILE_SIZE), dtype=dtype)
+    Bs = memref.view(
+        base, (TILE_SIZE, TILE_SIZE), dtype=dtype, shift=TILE_SIZE * TILE_SIZE
     )
-    B_shared = memref.global_(
-        sym_name="B_shared",
-        type=T.memref(TILE_SIZE, TILE_SIZE, T.f32(), memory_space=lds_space()),
-    )
-    kernel1_naive.emit()
-    kernel2_lds.emit()
+
+    # the inner row & col that we're accessing in this thread
+    tid = gpu.thread_id()
+    thread_row = tid / TILE_SIZE
+    thread_col = tid % TILE_SIZE
+
+    # the output block that we want to compute in this threadblock
+    c_row = block_idx.x * TILE_SIZE
+    c_col = block_idx.y * TILE_SIZE
+
+    tmp = arith.constant(0, type=dtype)
+
+    for bk_idx, tmp, _ in scf.range_(0, K, TILE_SIZE, iter_args=[tmp]):
+        A_ = A[c_row : c_row + TILE_SIZE, bk_idx : bk_idx + TILE_SIZE]
+        B_ = B[bk_idx : bk_idx + TILE_SIZE, c_col : c_col + TILE_SIZE]
+
+        # Have each thread load one of the elements in A & B
+        # Make the threadCol (=thread_idx.x) the consecutive index
+        # to allow global memory access coalescing
+        As[thread_row, thread_col] = A_[thread_row, thread_col]
+        Bs[thread_row, thread_col] = B_[thread_row, thread_col]
+
+        # block threads in this block until cache is fully populated
+        gpu.barrier()
+
+        # execute the dotproduct on the currently cached block
+        for dot_idx in range(TILE_SIZE):
+            a, b = As[thread_row, dot_idx], Bs[dot_idx, thread_col]
+            tmp = llvm.intr_fmuladd(a, b, tmp)
+
+        # need to sync again at the end, to avoid faster threads
+        # fetching the next block into the cache before slower threads are done
+        gpu.barrier()
+
+        tmp = yield tmp
+
+    C_ = C[c_row : c_row + TILE_SIZE, c_col : c_col + TILE_SIZE]
+    C_[thread_row, thread_col] = tmp
 
 
-gpu_module.operation.verify()
-print(gpu_module)
+launch_params[kernel2_lds_shared_subview.__name__] = (
+    (N // TILE_SIZE, N // TILE_SIZE, 1),
+    (TILE_SIZE, TILE_SIZE, 1),
+    2 * TILE_SIZE * TILE_SIZE * T.f32().width // 8,
+)
+
+
+BLOCK_SIZE = 256
+
+# Block Tile size
+BN = 128
+BM = 128
+# Number of Row or column we read per batch
+BK = 8
+
+A_shared = memref.global_(
+    sym_name="A_shared_BK_BM_1",
+    type=T.memref(BK, BM, T.f32(), memory_space=lds_space()),
+    alignment=16,
+)
+B_shared = memref.global_(
+    sym_name="B_shared_BK_BN_1",
+    type=T.memref(BK, BN, T.f32(), memory_space=lds_space()),
+    alignment=16,
+)
+
+
+@gpu_func(emit=True)
+@canonicalize(using=scf.canonicalizer)
+def kernel3_registers(
+    A: T.memref(M, K, T.f32()), B: T.memref(K, N, T.f32()), C: T.memref(M, N, T.f32())
+):
+    tid_x = thread_idx.x
+    bid_x = block_idx.x
+    bid_y = block_idx.y
+
+    # Thread Tile size
+    TN = 4
+    TM = 4
+
+    nbWaves = BLOCK_SIZE // 32
+    # Wave Tile size
+    WN = 64
+    WM = BN * BM // nbWaves // WN
+
+    # Number of wave on X & Y axis in the Block tile
+    nbWaveX = BN // WN
+
+    # A wave is a block of 8x4 of the output matrix
+    nbThreadXPerWave = 8
+    nbThreadYPerWave = 4
+
+    nbIterWaveN = WN // (nbThreadXPerWave * TN)
+    nbIterWaveM = WM // (nbThreadYPerWave * TM)
+
+    # Wave Sub-tile size
+    SUBWN = WN // nbIterWaveN
+    SUBWM = WM // nbIterWaveM
+
+    strideReadB = BLOCK_SIZE // BN
+    strideReadA = BLOCK_SIZE // BK
+    nbReadsB = BN * BK // BLOCK_SIZE
+    nbReadsA = BM * BK // BLOCK_SIZE
+
+    waveIndex = tid_x // 32
+    waveIdx = waveIndex % nbWaveX
+    waveIdy = waveIndex // nbWaveX
+    indexInWave = tid_x % 32
+
+    # Thread coordinates in Wave
+    idxInWave = indexInWave % nbThreadXPerWave
+    idyInWave = indexInWave / nbThreadXPerWave
+
+    # Thread mapping to read BKxBN block from A
+    rAIdx = tid_x % BK
+    rAIdy = tid_x // BK
+    # Thread mapping to read BNxBK block from B
+    rBIdx = tid_x % BN
+    rBIdy = tid_x // BN
+
+    A_col = memref.alloca([nbIterWaveM * TM], T.f32())
+    B_row = memref.alloca([nbIterWaveN * TN], T.f32())
+
+    As = memref.get_global(A_shared)
+    Bs = memref.get_global(B_shared)
+
+    l = TM * nbIterWaveM * TN * nbIterWaveN
+    c_regs = memref.alloca([l], T.f32())
+    for i in range(l):
+        c_regs[i] = 0.0
+
+    for kId in scf.range_(0, N, BK):
+
+        for i in range(nbReadsB):
+            index_x = BN * bid_x + rBIdx
+            index_y = rBIdy + i * strideReadB + kId
+            Bs[index_y % BK, index_x % BN] = B[index_y, index_x]
+
+        for i in range(nbReadsA):
+            index_x = rAIdx + kId
+            index_y = BM * bid_y + rAIdy + i * strideReadA
+            As[index_x % BK, index_y % BM] = A[index_y, index_x]
+
+        gpu.barrier()
+
+        for k in range(BK):
+            for iterWave in range(nbIterWaveN):
+                for i in range(TN):
+                    index = waveIdx * WN + iterWave * SUBWN + TN * idxInWave + i
+                    B_row[iterWave * TN + i] = Bs[k, index]
+
+            for iterWave in range(nbIterWaveM):
+                for i in range(TM):
+                    index = waveIdy * WM + iterWave * SUBWM + TM * idyInWave + i
+                    A_col[iterWave * TM + i] = As[k, index]
+
+            for iterWaveM in range(nbIterWaveM):
+                for iterWaveN in range(nbIterWaveN):
+                    for yt in range(TM):
+                        for xt in range(TN):
+                            x = iterWaveN * TN + xt
+                            y = iterWaveM * TM + yt
+                            a = A_col[y]
+                            b = B_row[x]
+                            c = c_regs[y * TN * nbIterWaveN + x]
+                            c = llvm.intr_fmuladd(a, b, c)
+                            c_regs[y * TN * nbIterWaveN + x] = c
+
+        gpu.barrier()
+
+    for iterWaveM in range(nbIterWaveM):
+        for iterWaveN in range(nbIterWaveN):
+            xOut = bid_x * BN + waveIdx * WN + iterWaveN * SUBWN + TN * idxInWave
+            yOut = bid_y * BM + waveIdy * WM + iterWaveM * SUBWM + TM * idyInWave
+            for yt in range(TM):
+                for xt in range(TN):
+                    C[yOut + yt, xOut + xt] = c_regs[
+                        TN * nbIterWaveN * (iterWaveM * TM + yt) + (iterWaveN * TN + xt)
+                    ]
+
+
+launch_params[kernel3_registers.__name__] = (
+    (N // 128, N // 128, 1),
+    (BLOCK_SIZE, 1, 1),
+    0,
+)
+
+ip.__exit__(None, None, None)
+
+simplified_module = run_pipeline(
+    ctx.module,
+    Pipeline()
+    .canonicalize()
+    .cse()
+    .loop_invariant_code_motion()
+    .loop_invariant_subset_hoisting()
+    .rocdl_attach_target(chip=arch, O=3),
+)
+
+# print(simplified_module)
 
 lowered_module = run_pipeline(
-    gpu_module,
+    simplified_module,
     Pipeline()
     .Gpu(Pipeline().convert_gpu_to_rocdl(use_bare_ptr_memref_call_conv=True))
-    .rocdl_attach_target(chip=arch)
     .gpu_to_llvm()
     .lower_to_llvm(),
 )
 
-print(lowered_module)
+# print(lowered_module)
+
+
+kernel_funcs = find_ops(
+    lowered_module.operation, lambda o: isinstance(o.opview, llvm.LLVMFuncOp)
+)
+for k in kernel_funcs:
+    _, thread_dims, _ = launch_params[k.sym_name.value]
+    k.attributes["rocdl.max_flat_work_group_size"] = IntegerAttr.get(
+        T.index(), np.prod(thread_dims)
+    )
+
+lowered_module_llvm = run_pipeline(
+    lowered_module, Pipeline().gpu_module_to_binary(format="llvm")
+)
+hsaco = get_compile_object_bytes(lowered_module_llvm)
+with open("llvm.bc", "wb") as f:
+    f.write(hsaco)
+# subprocess.check_call(
+#     [
+#         "/home/mlevental/dev_projects/llvm-project/cmake-build-debug/bin/llvm-dis",
+#         "/home/mlevental/dev_projects/mlir-python-extras/examples/llvm.bc",
+#         "-o",
+#         "/home/mlevental/dev_projects/fp32_sgemm_amd/src/llvm.ll",
+#     ]
+# )
+
 
 lowered_module = run_pipeline(lowered_module, Pipeline().gpu_module_to_binary())
-
 hsaco = get_compile_object_bytes(lowered_module)
 hip_module = hip_check(hip.hipModuleLoadData(hsaco))
 
 a_h = np.random.randint(0, 10, (M, K)).astype(dtype=np.float32)
 b_h = np.random.randint(0, 10, (K, N)).astype(dtype=np.float32)
+# a_h = np.ones((M, K)).astype(dtype=np.float32)
+# b_h = np.ones((M, K)).astype(dtype=np.float32)
 
 a_num_bytes = a_h.size * a_h.itemsize
 b_num_bytes = b_h.size * b_h.itemsize
@@ -144,24 +447,33 @@ b_d = hip_check(hip.hipMalloc(b_num_bytes))
 hip_check(hip.hipMemcpy(a_d, a_h, a_num_bytes, hip.hipMemcpyKind.hipMemcpyHostToDevice))
 hip_check(hip.hipMemcpy(b_d, b_h, b_num_bytes, hip.hipMemcpyKind.hipMemcpyHostToDevice))
 
-blocks_per_grid_x = max(M // TILE_SIZE, 1)
-blocks_per_grid_y = max(N // TILE_SIZE, 1)
-blocks_per_grid_z = 1
-threads_per_block_x = TILE_SIZE
-threads_per_block_y = TILE_SIZE
 stream = 0
-shared_memory = 0
 
 times = {
     kernel1_naive.__name__: 0,
-    kernel2_lds.__name__: 0,
+    kernel2_lds_shared_subview.__name__: 0,
+    kernel2_lds_shared_direct_dynamic.__name__: 0,
+    kernel2_lds_shared_direct_load_globals.__name__: 0,
+    kernel3_registers.__name__: 0,
 }
-runs = 10
-start, stop = hip.hipEventCreate(), hip.hipEventCreate()
-kernels = [kernel1_naive, kernel2_lds]
+runs = 16
+kernels = [
+    kernel1_naive,
+    kernel2_lds_shared_direct_load_globals,
+    kernel2_lds_shared_direct_dynamic,
+    kernel2_lds_shared_subview,
+    kernel3_registers,
+]
+# random.shuffle(kernels)
 for kernel in kernels:
     for i in range(runs):
-        # random.shuffle(kernels)
+        c_h = -3 * np.ones((M, N), dtype=np.float32)
+        c_num_bytes = c_h.size * c_h.itemsize
+        function = hip_check(
+            hip.hipModuleGetFunction(hip_module, kernel.__name__.encode())
+        )
+        hip_check(hip.hipDeviceSynchronize())
+
         hip_check(
             hip.hipMemcpy(
                 a_d, a_h, a_num_bytes, hip.hipMemcpyKind.hipMemcpyHostToDevice
@@ -173,8 +485,6 @@ for kernel in kernels:
             )
         )
 
-        c_h = -3 * np.ones((M, N), dtype=np.float32)
-        c_num_bytes = c_h.size * c_h.itemsize
         c_d = hip_check(hip.hipMalloc(c_num_bytes))
         hip_check(
             hip.hipMemcpy(
@@ -182,25 +492,34 @@ for kernel in kernels:
             )
         )
 
-        function = hip_check(
-            hip.hipModuleGetFunction(hip_module, kernel.__name__.encode())
-        )
+        (
+            (
+                blocks_per_grid_x,
+                blocks_per_grid_y,
+                blocks_per_grid_z,
+            ),
+            (
+                threads_per_block_x,
+                threads_per_block_y,
+                threads_per_block_z,
+            ),
+            shared_memory,
+        ) = launch_params[kernel.__name__]
 
-        start = time.monotonic()
-        launch_kernel(
+        time_compute = launch_kernel(
             function.as_c_void_p(),
             blocks_per_grid_x,
             blocks_per_grid_y,
             blocks_per_grid_z,
             threads_per_block_x,
             threads_per_block_y,
+            threads_per_block_z,
             stream,
             shared_memory,
             a_d,
             b_d,
             c_d,
         )
-        hip_synchronize()
 
         hip_check(
             hip.hipMemcpy(
@@ -209,16 +528,17 @@ for kernel in kernels:
         )
         correct = a_h @ b_h
         if not np.allclose(correct, c_h):
+            # with np.printoptions(threshold=np.inf, linewidth=np.inf):
             # print(correct)
             # print(c_h)
             print(f"{kernel.__name__} failed")
-        else:
-            if i > 0:
-                times[kernel.__name__] += time.monotonic() - start
-                print(f"{kernel.__name__} : {times[kernel.__name__]}")
+
+        times[kernel.__name__] += time_compute
+
+        print(f"{kernel.__name__} : {time_compute}")
 
 for k in times:
     times[k] /= runs
 
 for k, v in times.items():
-    print(f"{k}: {v:.3e}ms")
+    print(f"{k}: {v}ms")
